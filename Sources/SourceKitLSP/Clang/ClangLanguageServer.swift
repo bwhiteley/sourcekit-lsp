c8d9131de5a72be5c735fa7f73bc21c64f71d5db
Richard Howell <rhow@fb.com>
2022-10-12 14:57:01 -0700

    add openInterface() language server method

--- Sources/SourceKitLSP/Clang/ClangLanguageServer.swift
+++ Sources/SourceKitLSP/Clang/ClangLanguageServer.swift
@@ -524,6 +524,10 @@ extension ClangLanguageServerShim {
     }
   }
 
+  func openInterface(_ request: Request<OpenInterfaceRequest>) {
+    request.reply(.failure(.unknown("unsupported method")))
+  }
+
   // MARK: - Other
 
   func executeCommand(_ req: Request<ExecuteCommandRequest>) {
---
+++ Sources/SourceKitLSP/Swift/OpenInterface.swift
@@ -0,0 +1,88 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the Swift.org open source project
+//
+// Copyright (c) 2014 - 2022 Apple Inc. and the Swift project authors
+// Licensed under Apache License v2.0 with Runtime Library Exception
+//
+// See https://swift.org/LICENSE.txt for license information
+// See https://swift.org/CONTRIBUTORS.txt for the list of Swift project authors
+//
+//===----------------------------------------------------------------------===//
+
+import Foundation
+import SourceKitD
+import LanguageServerProtocol
+import LSPLogging
+
+struct InterfaceInfo {
+  var contents: String?
+}
+
+private var textualInterfaces: [String:DocumentURI] = [:]
+
+extension SwiftLanguageServer {
+
+  public func openInterface(_ request: LanguageServerProtocol.Request<LanguageServerProtocol.OpenInterfaceRequest>) {
+    let uri = request.params.textDocument.uri
+    let moduleName = request.params.name
+    let uuid = NSUUID().uuidString
+        
+    self.queue.async {
+      
+      if let uri = textualInterfaces[moduleName] {
+        request.reply(.success(InterfaceDetails(uri: uri)))
+        return
+      }
+      
+      self._openInterface(uri, moduleName, uuid) { result in
+        guard let interfaceInfo = result.success ?? nil else {
+          if let error = result.failure {
+            log("open interface failed: \(error)", level: .warning)
+          }
+          request.reply(.failure(ResponseError(result.failure!)))
+          return
+        }
+        
+        // FIXME: use some configurable path here
+        let path = NSTemporaryDirectory() + "/" + uuid + ".swift"
+        do {
+          try interfaceInfo.contents?.write(toFile: path, atomically: true, encoding: String.Encoding.utf8)
+        } catch {
+          request.reply(.failure(ResponseError.unknown(error.localizedDescription)))
+        }
+        
+        let uri = DocumentURI(URL(fileURLWithPath: path))
+        textualInterfaces[moduleName] = uri
+        request.reply(.success(InterfaceDetails(uri: uri)))
+      }
+    }
+  }
+  
+  private func _openInterface(_ uri: DocumentURI,
+                              _ name: String,
+                              _ uuid: String,
+                              _ completion: @escaping (Swift.Result<InterfaceInfo?, SKDError>) -> Void) {
+    let keys = self.keys
+    let skreq = SKDRequestDictionary(sourcekitd: sourcekitd)
+    skreq[keys.request] = requests.editor_open_interface
+    skreq[keys.modulename] = name
+    skreq[keys.name] = uuid
+    skreq[keys.synthesizedextensions] = 1
+    if let compileCommand = self.commandsByFile[uri] {
+      skreq[keys.compilerargs] = compileCommand.compilerArgs
+    }
+
+    let handle = self.sourcekitd.send(skreq, self.queue) { result in
+      guard let dict = result.success else {
+        return completion(.failure(result.failure!))
+      }
+      
+      return completion(.success(InterfaceInfo(contents: dict[keys.sourcetext])))
+    }
+    
+    // FIXME: cancellation
+    _ = handle
+  }
+    
+}
